14b0ba1047cabb6a220299a1196ff3db54c46b9b
Waqas <waqas013075@gmail.com>
2025-02-27 16:42:17 +0500

    changes port accordingly

--- terraform-code/modules/vpc/main.tf
+++ terraform-code/modules/vpc/main.tf
@@ -101,10 +101,10 @@ resource "aws_security_group" "test_sg" {
 
 # Ingress Security Port 22
 resource "aws_security_group_rule" "ssh_inbound_access" {
-  from_port         = 22
+  from_port         = 443
   protocol          = "tcp"
   security_group_id = aws_security_group.test_sg.id
-  to_port           = 22
+  to_port           = 443
   type              = "ingress"
   cidr_blocks       = ["0.0.0.0/0"]
 }
